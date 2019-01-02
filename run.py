a061f2c0cec06f5c71afbc201be741b9a7b7dd3d
AntLouiz <luizrodrigo46@hotmail.com>
2019-01-01 23:36:40 -0300

    adding the crawler in a main function

--- run.py
+++ run.py
@@ -25,90 +25,94 @@ def download_image(client):
     ).click()
 
 
-"""
-Coordenadas de Parnaíba:
-"""
-coordinates = {
-    'lat': -2.9055,
-    'long': -41.7734
-}
+def run_webcrawler():
 
-credentials = {
-    'username': username,
-    'password': password
-}
+    # Coordenadas de Parnaíba:
+    coordinates = {
+        'lat': -2.9055,
+        'long': -41.7734
+    }
 
-response = client.get(base_url)
+    credentials = {
+        'username': username,
+        'password': password
+    }
 
-wait(client, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div.ui-dialog:nth-child(17) > div:nth-child(3) > div:nth-child(1) > button:nth-child(1)"))).click()
+    response = client.get(base_url)
 
-coordinate_button = client.find_element_by_xpath("//div[@id='lat_lon_section']/label[2]")
-coordinate_button.click()
+    wait(client, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div.ui-dialog:nth-child(17) > div:nth-child(3) > div:nth-child(1) > button:nth-child(1)"))).click()
+
+    coordinate_button = client.find_element_by_xpath("//div[@id='lat_lon_section']/label[2]")
+    coordinate_button.click()
 
 
-client.find_element_by_xpath(
-    "//input[@id='coordEntryAdd']"
-).click()
+    client.find_element_by_xpath(
+        "//input[@id='coordEntryAdd']"
+    ).click()
 
-input_lat = client.find_element_by_xpath(
-    "//div[@aria-describedby='coordEntryDialogArea']//input[@class='latitude txtbox decimalBox']"
-)
+    input_lat = client.find_element_by_xpath(
+        "//div[@aria-describedby='coordEntryDialogArea']//input[@class='latitude txtbox decimalBox']"
+    )
 
-input_long = client.find_element_by_xpath(
-    "//div[@aria-describedby='coordEntryDialogArea']//input[@class='longitude txtbox decimalBox']"
-)
+    input_long = client.find_element_by_xpath(
+        "//div[@aria-describedby='coordEntryDialogArea']//input[@class='longitude txtbox decimalBox']"
+    )
 
-client.implicitly_wait(2)
+    client.implicitly_wait(2)
 
-input_lat.send_keys(
-    str(coordinates['lat'])
-)
+    input_lat.send_keys(
+        str(coordinates['lat'])
+    )
 
-input_long.send_keys(
-    str(coordinates['long'])
-)
+    input_long.send_keys(
+        str(coordinates['long'])
+    )
 
-client.find_element_by_xpath(
-    "//div[@id='coordEntryDialogArea']/..//span[text()='Add']"
-).click()
+    client.find_element_by_xpath(
+        "//div[@id='coordEntryDialogArea']/..//span[text()='Add']"
+    ).click()
 
-client.implicitly_wait(2)
+    client.implicitly_wait(2)
 
-client.find_element_by_xpath(
-    "//input[@value='Data Sets »']"
-).click()
+    client.find_element_by_xpath(
+        "//input[@value='Data Sets »']"
+    ).click()
 
-client.implicitly_wait(5)
+    client.implicitly_wait(5)
 
-client.find_element_by_xpath("//li[@id='cat_210']/div").click()
+    client.find_element_by_xpath("//li[@id='cat_210']/div").click()
 
-client.find_element_by_xpath(
-    "//span[@title='Landsat Collection 1 Standard Level-1 Scene Products']"
-).click()
+    client.find_element_by_xpath(
+        "//span[@title='Landsat Collection 1 Standard Level-1 Scene Products']"
+    ).click()
 
-client.find_element_by_xpath(
-    "//input[@id='coll_12864']"
-).click()
+    client.find_element_by_xpath(
+        "//input[@id='coll_12864']"
+    ).click()
 
-client.find_element_by_xpath(
-    "//form[@name='dataSetForm']//input[@value='Results »']"
-).click()
+    client.find_element_by_xpath(
+        "//form[@name='dataSetForm']//input[@value='Results »']"
+    ).click()
 
-download_image(client)
-
-login_button = client.find_element_by_xpath("//input[@value='Login']")
-
-if login_button:
-    login_button.click()
-
-    client.implicitly_wait(10)
-
-    make_login(client, credentials)
     download_image(client)
 
+    login_button = client.find_element_by_xpath("//input[@value='Login']")
 
-download_button = client.find_element_by_xpath(
-    "//*[@id='optionsPage']/div[1]/div[4]/input"
-)
+    if login_button:
+        login_button.click()
 
-download_button.click()
+        client.implicitly_wait(10)
+
+        make_login(client, credentials)
+        download_image(client)
+
+
+    download_button = client.find_element_by_xpath(
+        "//*[@id='optionsPage']/div[1]/div[4]/input"
+    )
+
+    download_button.click()
+
+
+if __name__ == '__main__':
+    run_webcrawler()
