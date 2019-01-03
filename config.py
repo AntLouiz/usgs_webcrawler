5ff028a997dc192c42ff005354ca2642d45cdca5
AntLouiz <luizrodrigo46@hotmail.com>
2019-01-03 11:41:06 -0300

    directories settings and config updated

--- config.py
+++ config.py
@@ -1,18 +1,24 @@
 import os.path
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from settings import BASE_DIR as base_dir
-from settings import HEADLESS
+from settings import (
+    BASE_DIR,
+    DOWNLOAD_DIR,
+    TEMP_DIR,
+    HEADLESS
+)
 
 profile = webdriver.FirefoxProfile()
 profile.set_preference('browser.download.folderList', 2)
-profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/zip')
+profile.set_preference(
+    'browser.helperApps.neverAsk.saveToDisk',
+    'application/zip'
+)
 
-download_dir = os.path.join(base_dir, 'downloads/')
-profile.set_preference('browser.download.dir', download_dir)
+temp_dir = os.path.join(BASE_DIR, TEMP_DIR)
+download_dir = os.path.join(BASE_DIR, DOWNLOAD_DIR)
+profile.set_preference('browser.download.dir', temp_dir)
 
 
 options = Options()
 options.set_headless(True)
-
-client = webdriver.Firefox(firefox_profile=profile, options=options)
--- settings.py
+++ settings.py
@@ -6,3 +6,8 @@ USGS_PASSWORD = config('USGS_PASSWORD')
 BASE_URL = 'https://earthexplorer.usgs.gov/'
 BASE_DIR = os.path.abspath('./')
 HEADLESS = config('HEADLESS', default=True)
+TEMP_DIR = '_temp/'
+DOWNLOAD_DIR = config(
+    'DOWNLOAD_DIR',
+    default='downloads/'
+)
