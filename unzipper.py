451854f89759c1474139e28f80bcf12de9a2e0be
AntLouiz <luizrodrigo46@hotmail.com>
2019-01-03 11:41:36 -0300

    parameters updated on clean_file function

--- unzipper.py
+++ unzipper.py
@@ -3,12 +3,12 @@ import glob
 import os
 
 
-def clean_file(file_path, extract_dir='./'):
+def clean_file(file_path, output_dir='./'):
     with zipfile.ZipFile(file_path, 'r') as zip_ref:
-        zip_ref.extractall(extract_dir)
+        zip_ref.extractall(output_dir)
 
-    tir_file = glob.glob("{}/*TIR.tif".format(extract_dir))
-    all_files = glob.glob("{}/*.tif".format(extract_dir))
+    tir_file = glob.glob("{}/*TIR.tif".format(output_dir))
+    all_files = glob.glob("{}/*.tif".format(output_dir))
 
     files_to_exclude = [file for file in all_files if file not in tir_file]
 
