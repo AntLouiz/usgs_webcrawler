c40f73c6d42240dd4131ba4be0d99c8b2613a758
AntLouiz <luizrodrigo46@hotmail.com>
2019-01-08 21:37:52 -0300

    create the _temp and downloads directory if doesn't exists

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os.path
+import os
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from settings import (
@@ -16,7 +17,15 @@ profile.set_preference(
 )
 
 temp_dir = os.path.join(BASE_DIR, TEMP_DIR)
+
+if not os.path.exists(temp_dir):
+    os.makedirs(temp_dir)
+
 download_dir = os.path.join(BASE_DIR, DOWNLOAD_DIR)
+
+if not os.path.exists(download_dir):
+    os.makedirs(download_dir)
+
 profile.set_preference('browser.download.dir', temp_dir)
 
 
