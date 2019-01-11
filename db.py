921753ee7f9f5ec780e9621c7b0068b5c0c444c0
AntLouiz <luizrodrigo46@hotmail.com>
2019-01-11 09:14:14 -0300

    Session for the ORM added to db module

--- db.py
+++ db.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from settings import DB_URL as db_url
 
 
 engine = create_engine(db_url)
 conn = engine.connect()
+Session = sessionmaker(bind=engine)
+session = Session()
