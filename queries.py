068bb68d6d48a6b174c9494c8f4a7d2ce657f44a
AntLouiz <luizrodrigo46@hotmail.com>
2019-01-11 16:18:40 -0300

    get_scraping_orders refactored using the sqlalchemy ORM

--- queries.py
+++ queries.py
@@ -1,8 +1,76 @@
-from sqlalchemy import select, MetaData, Table
-from db import conn, engine
+from sqlalchemy import update
+from sqlalchemy import MetaData, Table
+from db import conn, engine, session
+
+
+def result_to_dict(keys, result):
+    result_dict = dict(zip(keys, result))
+
+    return result_dict
 
 
 def get_scraping_orders():
+    metadata = MetaData(bind=None)
+    ScrapingOrder = Table(
+        'core_scrapingorder',
+        metadata,
+        autoload=True,
+        autoload_with=engine
+    )
+
+    Coordinates = Table(
+        'core_coordinates',
+        metadata,
+        autoload=True,
+        autoload_with=engine
+    )
+
+    Shapefiles = Table(
+        'core_shapefile',
+        metadata,
+        autoload=True,
+        autoload_with=engine
+    )
+
+    query_fields = {
+        'status': ScrapingOrder.c.status,
+        'latitude': Coordinates.c.latitude,
+        'longitude': Coordinates.c.longitude,
+        'shapefile_key': Shapefiles.c.key,
+        'id': ScrapingOrder.c.key
+    }
+
+    filter_fields = {
+        'scrapping_coord_id': ScrapingOrder.c.coordinates_id,
+        'coordinate_id': Coordinates.c.id,
+        'coord_shape_id': Coordinates.c.shapefile_id,
+        'shapefile_id': Shapefiles.c.id
+    }
+
+    query_results = session.query(
+        *query_fields.values()
+    ).filter(
+        filter_fields['scrapping_coord_id'] == filter_fields['coordinate_id']
+    ).filter(
+        filter_fields['coord_shape_id'] == filter_fields['shapefile_id']
+    ).filter(
+        query_fields['status'] == 'waiting'
+    ).all()
+
+    results = []
+
+    for result in query_results:
+        results.append(
+            result_to_dict(
+                list(query_fields.keys()),
+                result
+            )
+        )
+
+    return results
+
+
+def update_scraping_order(key, status='no_result'):
     metadata = MetaData(bind=None)
     scrapingorder_table = Table(
         'core_scrapingorder',
@@ -11,10 +79,12 @@ def get_scraping_orders():
         autoload_with=engine
     )
 
-    query_select = select(
-        [scrapingorder_table]
+    query_update = update(
+        scrapingorder_table
+    ).where(
+        scrapingorder_table.c.key == key
+    ).values(
+        status=status
     )
 
-    result = conn.execute(query_select)
-
-    return result
+    conn.execute(query_update)
