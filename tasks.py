17fdebdc1c096f4c31868554d19f30ae18b90ca7
AntLouiz <luizrodrigo46@hotmail.com>
2019-01-22 16:39:55 -0300

    logger added to the tasks module

--- tasks.py
+++ tasks.py
@@ -2,6 +2,7 @@ import glob
 import os.path
 from config import temp_dir
 from celery import Celery
+from celery.utils.log import get_task_logger
 from spider import execute_scraping_order
 from uploader import get_shapefile
 from settings import TEMP_DIR, BROKER_URL as broker_url
@@ -11,11 +12,11 @@ from queries import (
 )
 
 app = Celery('tasks', broker=broker_url)
-
+logger = get_task_logger(__name__)
 
 @app.task
 def crawl_order(order):
-    print(order)
+    logger.info(order)
     get_shapefile(
         order['shapefile_key']
     )
